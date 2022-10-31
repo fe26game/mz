e6a3857227633f3e75cb0bd2e7c1a57c53f34898
luxiuyuan2013 <111162347+luxiuyuan2013@users.noreply.github.com>
2022-10-31 12:44:26 +0800

    Add files via upload

---
+++ 私信魔怔系统.user.js
@@ -0,0 +1,29 @@
+﻿// ==UserScript==
+// @name         私信魔怔系统
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  None
+// @author       Luxiuyuan2013
+// @match        https://*.luogu.com.cn/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=luogu.com.cn
+// @grant        none
+// ==/UserScript==
+
+async function sendMessage(uid,content){
+    await fetch("https://www.luogu.com.cn/api/chat/new", {
+        headers: [
+            ["content-type", "application/json"],
+            ["referer", "https://www.luogu.com.cn/"],
+            ["x-csrf-token", document.querySelector("meta[name=csrf-token]").content],
+        ],
+        body: JSON.stringify({
+            user: uid,
+            content: content,
+        }),
+        method: "POST",
+    });
+}
+
+(function() {
+    sendMessage(685826,"你好，陆修远！");
+})();
